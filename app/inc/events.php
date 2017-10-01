3a017ca3b0ba76e7e0231ee6c2f142c32fbc9b55
Axel Nana <ax.lnana@outlook.com>
2017-10-01 17:12:32 +0200

    Uncomment example and add one core event callback

--- app/inc/events.php
+++ app/inc/events.php
@@ -12,6 +12,20 @@
      * the main controller instance.
      *
      */
-    // add_event_callback('controller', 'init', function( &$controller ) {
+    add_event_callback('controller', 'init', function( &$controller ) {
         // YOUR FUNCTION
-    // });
+    });
+
+    /*
+     *----------------------------------------------------------------------
+     * LANGUAGE CHANGE CALLBACK
+     *----------------------------------------------------------------------
+     *
+     * This function will be called just after the change of the language files
+     * used. Note that this function has one parameter which represent the
+     * short name of the new language (ex: en or fr).
+     *
+     */
+    add_event_callback('lang', 'change', function( $language ) {
+        // YOUR FUNCTION
+    });
