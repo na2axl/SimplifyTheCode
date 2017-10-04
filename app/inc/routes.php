dc217005555045dda836c4b30f246d99d3e02978
Axel Nana <ax.lnana@outlook.com>
2017-10-04 21:15:42 +0200

    Adding more functionalities in the example project

--- app/inc/routes.php
+++ app/inc/routes.php
@@ -2,4 +2,9 @@
 
     defined('BASEPATH') OR exit('No direct script access allowed');
 
+    // The defult controller
     $routes['default'] = 'Home';
+
+    // This controller will be called to switch the language
+    $routes['lang/(:str)'] = 'Tools/setLang/$1';
+    $routes['lang/(:str)/(:any)'] = 'Tools/setLang/$1/$2';
