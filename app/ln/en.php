0e613946686cc1124c64ef820abc7c9aeac5dd3f
Axel Nana <ax.lnana@outlook.com>
2017-10-17 00:45:58 +0200

    Delete files

--- app/ln/en.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    defined('BASEPATH') OR exit('No direct script access allowed');
-
-    $lang['lang_name'] = 'English';
-    $lang['lang_name_short'] = 'en';
-
-    $lang['welcome'] = 'Welcome to <b>SimplifyTheCode</b>';
-    $lang['welcome_info'] = 'Page generated in %sms using %so of memory.';
--- app/ln/fr.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    defined('BASEPATH') OR exit('No direct script access allowed');
-
-    $lang['lang_name'] = 'Français';
-    $lang['lang_name_short'] = 'fr';
-
-    $lang['welcome'] = 'Bienvenue sur <b>SimplifyTheCode</b>';
-    $lang['welcome_info'] = 'Page générée en %sms utilisant %so de mémoire.';
